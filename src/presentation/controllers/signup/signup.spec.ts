94b7131d9e7b5fde261257e11d835f30def026aa
Renan Eduardo F. da Silva <renaneduardo1@hotmail.com>
2020-10-30 13:42:47 -0300

    test: ensure SignUpController returns 400 if validation fails

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -183,12 +183,19 @@ describe('Signup Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(ok(makeFakeAccount()))
   })
-})
 
-test('Should call Validation with correct values', async () => {
-  const { sut, validationStub } = makeSut()
-  const addSpy = jest.spyOn(validationStub, 'validate')
-  const httpRequest = makeFakeRequest()
-  await sut.handle(httpRequest)
-  expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const addSpy = jest.spyOn(validationStub, 'validate')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
