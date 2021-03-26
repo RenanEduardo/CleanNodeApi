0e5713977d9f49bb6595504ff3fa115ea1521895
Renan Eduardo F. da Silva <renaneduardo1@hotmail.com>
2021-03-26 17:26:41 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,12 +1,17 @@
-import { loginPath } from './paths/login-path'
-import { accountSchema } from './schemas/account-schema'
-import { loginParamsSchema } from './schemas/login-params-schema'
+import { loginPath } from './paths'
+import { unauthorized, badRequest, serverError, notFound } from './components'
+import { accountSchema, errorSchema, loginParamsSchema } from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do curso do Mango para realizar enquete entre programadores',
     version: '1.0.0'
+  },
+  license: {
+    name: 'MIT',
+    url: 'https://opensource.org/licenses/MIT'
   },
   servers: [{
     url: '/api'
@@ -19,6 +24,13 @@ export default {
   },
   schemas: {
     account: accountSchema,
-    loginParams: loginParamsSchema
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest: badRequest,
+    serverError: serverError,
+    unauthorized: unauthorized,
+    notFound: notFound
   }
 }
